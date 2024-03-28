c09d1021cec7e60d33f8983fd0f8d256c7c116c3
yusufadji <43660473+yusufadji@users.noreply.github.com>
2024-03-28 19:44:42 +0700

    fix: file list array didn't sorted on ubuntu

--- aniwebpmaker.py
+++ aniwebpmaker.py
@@ -14,7 +14,7 @@ offsetY = 0
 
 
 file_list = [file for file in os.listdir(src_folder) if file.endswith(file_extension)]
-
+file_list.sort()
 
 input_files = " ".join([f"-frame {os.path.join(src_folder, file)} +{duration}+{offsetX}+{offsetY}" for file in file_list])
 output_file = f"dst/{datetime.today().strftime('%Y%m%d%H%M%S')}.webp"
