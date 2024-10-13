61d177a1af7738c364172257a3bee53814f5e424
yusufadji <43660473+yusufadji@users.noreply.github.com>
2024-10-13 18:47:44 +0700

    fix: stop eternal loop

--- auto.py
+++ auto.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 
 
@@ -7,12 +8,20 @@ dst_folder = "dst"
 src_folder = "src"
 
 
+def extract_number(file_name):
+    numbers = re.findall(r'\d+', file_name)
+    return int(numbers[0]) if numbers else 0
+
+
 def compress_images(compression_quality):
-    file_list = [file for file in os.listdir(orig_folder) if file.endswith((".jpg", ".jpeg", ".png", ".webp"))]
+    file_list = [file for file in os.listdir(
+        orig_folder) if file.endswith((".jpg", ".jpeg", ".png", ".webp"))]
+    file_list = sorted(file_list, key=extract_number)
 
     for file in file_list:
         input_path = os.path.join(orig_folder, file)
-        output_path = os.path.join(src_folder, os.path.splitext(file)[0] + ".webp")
+        output_path = os.path.join(
+            src_folder, os.path.splitext(file)[0] + ".webp")
 
         command = f"cwebp -q {compression_quality} {input_path} -o {output_path}"
         os.system(command)
@@ -34,17 +43,22 @@ def auto_compress():
 
         make_webp()
 
-        file_list = [file for file in os.listdir(dst_folder) if file.endswith((".webp"))]
+        file_list = [file for file in os.listdir(
+            dst_folder) if file.endswith(".webp")]
+        file_list = sorted(file_list, key=extract_number)
         print(file_list)
         webp_file_path = f"dst/{file_list[0]}"
         file_size = get_file_size(webp_file_path)
 
-        if file_size > 512000:
-            if compression_quality >= 1:
-                compression_quality -= 3
-                print(f"compression_quality = {compression_quality}")
-                os.remove(f"dst/{file_list[0]}")
+        if file_size > 500000:
+            compression_quality -= 3
+            if compression_quality < 1:
+                print("Compression quality has reached below 1. Stopping program.")
+                break
+            print(f"compression_quality = {compression_quality}")
+            os.remove(f"dst/{file_list[0]}")
         else:
             break
+
 
 auto_compress()
